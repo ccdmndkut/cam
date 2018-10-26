14dcedc44ddaacd28cc3894a75fb155844594643
ccdmndkut <37282688+ccdmndkut@users.noreply.github.com>
2018-10-26 11:45:03 -0400

    d

---
+++ main.js
@@ -0,0 +1,41 @@
+window.onload = function() {
+  document.addEventListener('contextmenu', event => event.preventDefault());
+
+  var u = "camw.html",
+    img = document.getElementById("vidb"),
+    n = "myNewWindow",
+    w = img.clientWidth / 1.5,
+    h = (img.clientHeight - 4.5) / 1.5;
+
+  var myparameters =
+    "width=" +
+    w +
+    ", height=" +
+    h +
+    ", resizable = yes, toolbar = no, location = no, directories = no, status = no, menubar = no, scrollbars = no, resizable = no";
+
+  window.close();
+  var b = window.open(u, n, myparameters);
+  var c = window.focus();
+  return [a, b, c];
+};
+
+popper = function() {
+    var u = "camw.html",
+        img = document.getElementById("vidb"),
+        n = "myNewWindow",
+        w = img.clientWidth / 1.5,
+        h = (img.clientHeight - 4.5) / 1.5;
+
+    var myparameters =
+        "width=" +
+        w +
+        ", height=" +
+        h +
+        ", resizable = yes, toolbar = no, location = no, directories = no, status = no, menubar = no, scrollbars = no, resizable = no";
+
+    window.close();
+    var b = window.open(u, n, myparameters);
+    var c = window.focus();
+    return [a, b, c];
+}
